b9bbd8837229ba37a4118ffb8551a95d2abeb6d7
maxuna <maxuna0829@hotmail.com>
2014-04-05 00:29:17 +0200

    Merge branch 'master' of https://github.com/maxuna/GerdasSyhorna
    Conflicts:
    	GerdasSyhorna/FormCustomer.cs

--- GerdasSyhorna/FormCustomer.Designer.cs
+++ GerdasSyhorna/FormCustomer.Designer.cs
@@ -28,108 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBoxProduct1 = new System.Windows.Forms.PictureBox();
-            this.pictureBoxProduct2 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.buttonUp = new System.Windows.Forms.Button();
-            this.buttonDown = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProduct1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProduct2)).BeginInit();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
-            // pictureBoxProduct1
+            // flowLayoutPanel1
             // 
-            this.pictureBoxProduct1.Location = new System.Drawing.Point(39, 153);
-            this.pictureBoxProduct1.Name = "pictureBoxProduct1";
-            this.pictureBoxProduct1.Size = new System.Drawing.Size(127, 89);
-            this.pictureBoxProduct1.TabIndex = 0;
-            this.pictureBoxProduct1.TabStop = false;
-            // 
-            // pictureBoxProduct2
-            // 
-            this.pictureBoxProduct2.Location = new System.Drawing.Point(228, 153);
-            this.pictureBoxProduct2.Name = "pictureBoxProduct2";
-            this.pictureBoxProduct2.Size = new System.Drawing.Size(127, 89);
-            this.pictureBoxProduct2.TabIndex = 1;
-            this.pictureBoxProduct2.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(778, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
-            // buttonUp
-            // 
-            this.buttonUp.Location = new System.Drawing.Point(409, 94);
-            this.buttonUp.Name = "buttonUp";
-            this.buttonUp.Size = new System.Drawing.Size(75, 23);
-            this.buttonUp.TabIndex = 3;
-            this.buttonUp.Text = "button1";
-            this.buttonUp.UseVisualStyleBackColor = true;
-            // 
-            // buttonDown
-            // 
-            this.buttonDown.Location = new System.Drawing.Point(409, 303);
-            this.buttonDown.Name = "buttonDown";
-            this.buttonDown.Size = new System.Drawing.Size(75, 23);
-            this.buttonDown.TabIndex = 4;
-            this.buttonDown.Text = "button2";
-            this.buttonDown.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 265);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(276, 265);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 133);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(901, 374);
+            this.flowLayoutPanel1.TabIndex = 3;
             // 
             // FormCustomer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(925, 519);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.buttonDown);
-            this.Controls.Add(this.buttonUp);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBoxProduct2);
-            this.Controls.Add(this.pictureBoxProduct1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "FormCustomer";
             this.Text = "FormCustomer";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProduct1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProduct2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBoxProduct1;
-        private System.Windows.Forms.PictureBox pictureBoxProduct2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button buttonUp;
-        private System.Windows.Forms.Button buttonDown;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+
 
     }
 }
