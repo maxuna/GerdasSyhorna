51615a29198e2534ae57ed75a2e3fb48249c95b6
Bigfudge <simonpersson@flaskpost.org>
2014-05-09 09:00:32 +0200

    Ett steg fram, två steg bakåt

--- GerdasSyhorna/FormCustomer.cs
+++ GerdasSyhorna/FormCustomer.cs
@@ -13,15 +13,47 @@ namespace GerdasSyhorna
 {
     public partial class FormCustomer : Form
     {
-
+        int i = 0;
 
         public FormCustomer()
         {
             InitializeComponent();
 
-
+            List<Products> Products = new List<GerdasSyhorna.Products>();
+            List<Button> buyButtons = new List<Button>();
+            List<Label> nameLabel = new List<Label>();
 
             var database = Database.OpenConnection(Resources.connectionString);
+            var asas = database.Products;
+
+            foreach (var item in database.Products.All())
+            {
+                Products p = new Products(item.ProductId, item.ProductName, item.Category, item.Price, item.UnitsInStock, item.UnitsOnOrder, item.SupplierId, item.ImageFile, item.Description);
+                Products.Add(p);
+                Button b = new Button();
+                buyButtons.Add(b);
+                Label l = new Label();
+                nameLabel.Add(l);
+
+                flowLayoutPanel1.Controls.Add(((Products)Products[i]));
+                (((Products)Products[i]).Height) = 140;
+                (((Products)Products[i]).Width) = 900;
+                (((Products)Products[i]).BorderStyle) = BorderStyle.Fixed3D;
+
+                ((Products)Products[i]).Controls.Add(((Button)buyButtons[i]));
+                (((Button)buyButtons[i]).Height) = 75;
+                (((Button)buyButtons[i]).Width) = 140;
+                (((Button)buyButtons[i]).Location) = new Point(720, 35);
+                (((Button)buyButtons[i]).Image) = Image.FromFile("../../Images/Buy_Button.png");
+                ((Button)buyButtons[i]).Click += new EventHandler(buyButton_Click);
+
+                ((Products)Products[i]).Controls.Add(((Label)nameLabel[i]));
+                ((Label)nameLabel[i]).Text = "bajs";
+                ((Label)nameLabel[i]).Location = new Point(140, 5);
+                ((Label)nameLabel[i]).Font = new Font("Times New Roman", 12.0F);
+                ((Label)nameLabel[i]).AutoSize = true;
+                i++;
+            }
 
         }
 
--- GerdasSyhorna/Products.cs
+++ GerdasSyhorna/Products.cs
@@ -9,17 +9,19 @@ namespace GerdasSyhorna
     class Products : System.Windows.Forms.Label
     {
         public int index;
-        public int name;
+        public string name;
         public string category;
-        public double price;
-        public int unitsInStock;
-        public int unitsInOrder;
+        public object price;
+        public short unitsInStock;
+        public short unitsInOrder;
         public byte[] imageFile;
+        public object supplierId;
         public string description;
 
 
-        public Products(int index, int name, string category, double price, int unitsInStock, int unitsInOrder, byte[]imageFile, string description)
-        { 
+
+        public Products(int index, string name, string category, object price, short unitsInStock, short unitsInOrder, int? supplierId, byte[] imageFile, string description)
+        {
             this.index = index;
             this.name = name;
             this.category = category;
@@ -27,9 +29,10 @@ namespace GerdasSyhorna
             this.unitsInStock = unitsInStock;
             this.unitsInOrder = unitsInOrder;
             this.imageFile = imageFile;
+            this.supplierId = supplierId;
             this.description = description;
         }
-        public Products(int index, int name, string category, double price, int unitsInStock, int unitsInOrder, string description)
+        public Products(int index, string name, string category, object price, short unitsInStock, short unitsInOrder, int? supplierId, string description)
         {
             this.index = index;
             this.name = name;
