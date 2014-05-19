0c8f8880d7b35fb36ea827ae333b93101971a900
maxuna <maxuna0829@hotmail.com>
2014-05-19 12:47:00 +0200

    yuot

--- GerdasSyhorna/Form1.Designer.cs
+++ GerdasSyhorna/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.buttonLogIn = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxUsername
@@ -90,12 +91,23 @@
             this.linkLabel1.Text = "Skapa konto här";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(213, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 48);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Logga in för att \r\nkomma in på\r\nwebshopen";
+            // 
             // Form1
             // 
             this.AcceptButton = this.buttonLogIn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 169);
+            this.ClientSize = new System.Drawing.Size(336, 169);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.buttonLogIn);
             this.Controls.Add(this.label2);
@@ -120,6 +132,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonLogIn;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
